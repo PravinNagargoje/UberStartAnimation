c34684809d77dee7e8e82c26958fa5faa87bb2c7
Pravin <pravin@codetoart.com>
2017-02-13 16:17:04 +0530

    changed constraint format and other minor changes

--- PopUpAnimation/ViewController.swift
+++ PopUpAnimation/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    fileprivate var textField = UITextField()
-    fileprivate var viewOne = UIView()
-    fileprivate var viewTwo = UIView()
-    fileprivate var instructionLabel = UILabel()
-    fileprivate var uberLabel = UILabel()
-    fileprivate var backButton = UIButton()
+    fileprivate let textField = UITextField()
+    fileprivate let viewOne = UIView()
+    fileprivate let viewTwo = UIView()
+    fileprivate let instructionLabel = UILabel()
+    fileprivate let uberLabel = UILabel()
+    fileprivate let backButton = UIButton()
     fileprivate let accessoryView = AccessoryView()
-    fileprivate var showKeyboard = false
     fileprivate let distance: CGFloat = 32
+    fileprivate let placeholder = "Enter your mobile number"
+    
     fileprivate var labelTopConstraint: NSLayoutConstraint!
     fileprivate var textFieldTopConstraint: NSLayoutConstraint!
     fileprivate var topConstraint: NSLayoutConstraint!
-    fileprivate var placeholder = "Enter your mobile number"
+    fileprivate var showKeyboard = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,53 +59,17 @@ extension ViewController {
     
     func setupViewTwo() {
         self.viewTwo.translatesAutoresizingMaskIntoConstraints = false
-        
-        let leadingConstraint = NSLayoutConstraint(
-            item: viewTwo,
-            attribute: .leading,
-            relatedBy: .equal,
-            toItem: view,
-            attribute: .leading,
-            multiplier: 1.0,
-            constant: 0
+        NSLayoutConstraint.activate([
+            self.viewTwo.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.viewTwo.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.viewTwo.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+      
+        topConstraint = self.viewTwo.topAnchor.constraint(
+            equalTo: self.view.bottomAnchor,
+            constant: -self.view.frame.height/3
         )
-        let trailingConstraint = NSLayoutConstraint(
-            item: viewTwo,
-            attribute: .trailing,
-            relatedBy: .equal,
-            toItem: view,
-            attribute: .trailing,
-            multiplier: 1.0,
-            constant: 0
-        )
-       
-       let bottomConstraint = NSLayoutConstraint(
-            item: viewTwo,
-            attribute: .bottom,
-            relatedBy: .equal,
-            toItem: view,
-            attribute: .bottom,
-            multiplier: 1.0,
-            constant: 0.0
-        )
-        
-        topConstraint = NSLayoutConstraint(
-            item: viewTwo,
-            attribute: .top,
-            relatedBy: .equal,
-            toItem: view,
-            attribute: .bottom,
-            multiplier: 1.0,
-            constant: -275
-        )
-
-        view.addConstraints(
-            [leadingConstraint,
-             trailingConstraint,
-             topConstraint,
-             bottomConstraint
-            ]
-        )
+        topConstraint.isActive = true
     }
     
     func setupUberLabel() {
@@ -115,7 +80,7 @@ extension ViewController {
         
         self.uberLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.uberLabel.topAnchor.constraint(equalTo: self.viewTwo.topAnchor, constant: distance + 16 ),
+            self.uberLabel.topAnchor.constraint(equalTo: self.viewTwo.topAnchor, constant: distance + distance/2 ),
             self.uberLabel.leadingAnchor.constraint(equalTo: self.viewTwo.leadingAnchor, constant: distance)
         ])
     }
@@ -123,13 +88,6 @@ extension ViewController {
     func setupTextField() {
         self.viewTwo.addSubview(self.textField)
         self.viewTwo.addSubview(self.backButton)
-        
-//        let leftView : UILabel = UILabel(frame: CGRect(x: 10, y: 0, width: 7, height: 26))
-//        leftView.backgroundColor = UIColor.clear
-//        textField.leftView = leftView
-//        textField.leftViewMode = .always
-//        textField.contentVerticalAlignment = .center
-        
         textField.inputAccessoryView = accessoryView
         
         self.textField.placeholder = self.placeholder
@@ -137,27 +95,19 @@ extension ViewController {
         self.textField.keyboardType = .phonePad
         self.textField.keyboardAppearance = .dark
         self.textField.delegate = self
+
         self.textField.translatesAutoresizingMaskIntoConstraints = false
-        
-        textFieldTopConstraint = NSLayoutConstraint(
-            item: textField,
-            attribute: .top,
-            relatedBy: .equal,
-            toItem: backButton,
-            attribute: .bottom,
-            multiplier: 1.0,
-            constant: 16
-        )
-        
-        view.addConstraints(
-            [textFieldTopConstraint]
-        )
-        
         NSLayoutConstraint.activate([
             self.textField.leadingAnchor.constraint(equalTo: self.viewTwo.leadingAnchor, constant: distance ),
             self.textField.trailingAnchor.constraint(equalTo: self.viewTwo.trailingAnchor, constant: -distance),
             self.textField.heightAnchor.constraint(equalToConstant: 40)
         ])
+        
+        textFieldTopConstraint = self.textField.topAnchor.constraint(
+            equalTo: self.backButton.bottomAnchor,
+            constant: distance/2
+        )
+        textFieldTopConstraint.isActive = true
     }
     
     func setupBackButton() {
@@ -169,47 +119,46 @@ extension ViewController {
         self.backButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.backButton.topAnchor.constraint(equalTo: self.viewTwo.topAnchor, constant: distance),
-//            self.backButton.heightAnchor.constraint(equalToConstant: 40),
-            self.backButton.leadingAnchor.constraint(equalTo: self.viewTwo.leadingAnchor, constant: 16)
+            self.backButton.leadingAnchor.constraint(equalTo: self.viewTwo.leadingAnchor, constant: distance/2)
         ])
     }
     
     func setupLabel() {
         self.instructionLabel.text = self.placeholder
         self.viewTwo.addSubview(self.instructionLabel)
-        self.instructionLabel.font = UIFont(name: "Bold", size: 16)
+        self.instructionLabel.font = UIFont(name: "Bold", size: distance/2)
         self.instructionLabel.isHidden = true
         self.instructionLabel.textColor  = UIColor.lightGray
         self.instructionLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let leadingConstraint = NSLayoutConstraint(
-            item: instructionLabel,
-            attribute: .leading,
-            relatedBy: .equal,
-            toItem: viewTwo,
-            attribute: .leading,
-            multiplier: 1.0,
-            constant: 32
-        )
-        labelTopConstraint = NSLayoutConstraint(
-            item: instructionLabel,
-            attribute: .top,
-            relatedBy: .equal,
-            toItem: backButton,
-            attribute: .bottom,
-            multiplier: 1.0,
-            constant: 0
-        )
-        
-        view.addConstraints(
-            [leadingConstraint,
-             labelTopConstraint
-            ]
-        )
+        self.instructionLabel.leadingAnchor.constraint(
+            equalTo: self.viewTwo.leadingAnchor,
+            constant: distance
+            ).isActive = true
+        labelTopConstraint = self.instructionLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor)
+        labelTopConstraint.isActive = true
     }
     
     func backButtonClicked() {
         showKeyboard = false
+        self.topConstraint.constant = -self.view.frame.height/3
+        self.backButton.alpha = 0
+        self.setBottomBorder(color: .clear)
+        self.instructionLabel.isHidden = true
+        self.textFieldTopConstraint.constant = distance/2
+        self.textField.placeholder = self.placeholder
+        self.uberLabel.isHidden = false
+        UIView.animate(
+            withDuration: 0.8,
+            delay: 0,
+            usingSpringWithDamping: 1,
+            initialSpringVelocity: 0.5,
+            options: .curveEaseOut,
+            animations:
+            {
+                self.view.layoutIfNeeded()
+        })
+
         self.textField.resignFirstResponder()
     }
 }
@@ -219,10 +168,10 @@ extension ViewController: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         
         self.topConstraint.constant = -self.view.frame.size.height
-        self.textFieldTopConstraint.constant = distance*3 - 16
+        self.textFieldTopConstraint.constant = distance*3 - distance/2
         self.textField.placeholder = "(201) 555-5555"
         setBottomBorder(color: .black)
-        self.labelTopConstraint.constant = 32
+        self.labelTopConstraint.constant = distance
         self.uberLabel.isHidden = true
         self.instructionLabel.isHidden = false
         UIView.animate(
@@ -233,7 +182,7 @@ extension ViewController: UITextFieldDelegate {
             options: .curveEaseOut,
             animations:
             {
-            self.view.layoutIfNeeded()
+                self.view.layoutIfNeeded()
             }) { _ in
                 self.showKeyboard = true
                 self.backButton.alpha = 0.8
@@ -242,37 +191,12 @@ extension ViewController: UITextFieldDelegate {
           return showKeyboard
     }
     
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) ->Bool {
-        UIView.animate(
-            withDuration: 0.8,
-            delay: 0,
-            usingSpringWithDamping: 1,
-            initialSpringVelocity: 0.5,
-            options: .curveEaseOut,
-            animations:
-            {
-            self.topConstraint.constant = -275
-            self.backButton.alpha = 0
-            self.setBottomBorder(color: .clear)
-            self.instructionLabel.isHidden = true
-            self.textFieldTopConstraint.constant = 16
-            self.textField.placeholder = "Enter mobile number"
-            self.uberLabel.isHidden = false
-            self.view.layoutIfNeeded()
-        })
-        
-        return true
-    }
-    
     func textField(
         _ textField: UITextField,
         shouldChangeCharactersIn range: NSRange,
         replacementString string: String
         ) -> Bool
     {
-       
-        
         guard let text = textField.text else { return true }
         let newLength = text.characters.count + string.characters.count - range.length
         accessoryView.nextButton.isSelected = (newLength > 0)
@@ -280,7 +204,7 @@ extension ViewController: UITextFieldDelegate {
         if newLength == 0 {
             self.textField.text = ""
         }
-        return newLength > 0 ? true : false
+        return newLength <= 10 ? true : false
     }
 }
 
@@ -289,7 +213,6 @@ extension ViewController {
     func setBottomBorder(color: UIColor) {
         self.textField.borderStyle = .none
         self.textField.layer.backgroundColor = UIColor.white.cgColor
-        
         self.textField.layer.masksToBounds = false
         self.textField.layer.shadowColor = color.cgColor
         self.textField.layer.shadowOffset = CGSize(width: 0.0, height: 1.5)
